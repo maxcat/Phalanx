fea070cb7c5c244c6eb4f01e7094283336179825
maxcat <tanli1622@gmail.com>
2014-07-27 15:34:25 +0800

    -add the flow handler

--- Assets/Scripts/GroundControl.cs
+++ Assets/Scripts/GroundControl.cs
@@ -15,8 +15,11 @@ public class GroundControl : MonoBehaviour {
 	void Start () {
 		
 		// reset the last ground item index
-		m_lastGroundItemIndex = transform.GetChildCount() - 1;
-		
+		m_lastGroundItemIndex = transform.childCount - 1;
+
+		Flow main = new Flow(this, mainFlow());
+
+		main.start();
 	}
 	
 	// Update is called once per frame
@@ -39,10 +42,10 @@ public class GroundControl : MonoBehaviour {
 		if(GetLastItem().transform.localPosition.z < BOUNDARY_Z)
 		{
 			// over the boundary, move the item to the top
-			GetLastItem().transform.localPosition += new Vector3(0, 0, transform.GetChildCount() * ITEM_WIDTH);
+			GetLastItem().transform.localPosition += new Vector3(0, 0, transform.childCount * ITEM_WIDTH);
 			
 			// update the last index
-			m_lastGroundItemIndex = (m_lastGroundItemIndex + transform.GetChildCount() - 1) % transform.GetChildCount();
+			m_lastGroundItemIndex = (m_lastGroundItemIndex + transform.childCount) % transform.childCount;
 			
 			// tan li, pending
 			// update the last item's material
@@ -57,6 +60,34 @@ public class GroundControl : MonoBehaviour {
 	GameObject GetLastItem()
 	{
 		return transform.FindChild(m_lastGroundItemIndex.ToString()).gameObject;
+	}
+
+	IEnumerator mainFlow()
+	{
+		Flow flow1 = new Flow(this, testFlow(3));
+		Flow flow2 = new Flow(this, testFlow(2));
+
+		flow1.start();
+		yield return flow1.untilDone;
+
+		flow2.start();
+		yield return flow2.untilDone;
+
+		Debug.LogWarning("===flow 1 and flow 2 all finished===");
+
+		                 
+	}
+
+	IEnumerator testFlow(int time)
+	{
+		int i = 0;
+		while ( i < time)
+		{
+			Debug.LogWarning("=====flow wait for " + i  + " ====");
+			i ++;
+
+			yield return new WaitForSeconds(1.0f);
+		}
 	}
 	#endregion
 }
---
+++ Assets/Scripts/System/Flow.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Flow {
+
+	#region Fields
+	protected IEnumerator m_coroutine;
+	protected MonoBehaviour m_mono;
+
+
+	protected bool m_isPaused;
+	protected bool m_isRunning;
+	#endregion
+
+
+	#region Getter and Setter
+	public IEnumerator coroutine{
+		get { return m_coroutine;}
+		set { m_coroutine = value;}
+	}
+
+	public MonoBehaviour mono 
+	{
+		get { return m_mono;}
+		set { m_mono = value;}
+	}
+
+	public bool isRunning
+	{
+		get { return m_isRunning;}
+	}
+
+	public bool isPaused
+	{
+		get { return m_isPaused;}
+	}
+
+	public virtual Coroutine untilDone
+	{
+		get
+		{
+			return m_mono.StartCoroutine(untilDoneFlow());
+		}
+	}
+	#endregion
+
+	#region Constructor
+	public Flow()
+	{
+		m_coroutine = null;
+		m_mono = null;
+
+		m_isRunning = false;
+		m_isPaused = false;
+	}
+
+	public Flow(MonoBehaviour mono, IEnumerator coroutine)
+	{
+		m_coroutine = coroutine;
+		m_mono = mono;
+
+		m_isRunning = false;
+		m_isPaused = false;
+	}
+	#endregion
+
+
+	#region Public API
+	public virtual void start()
+	{
+		m_isRunning = true;
+		m_isPaused = false;
+
+		m_mono.StartCoroutine(doFlow());
+	}
+
+	public virtual void pause()
+	{
+		m_isPaused = true;
+	}
+
+	public virtual void unPause()
+	{
+		m_isPaused = false;
+	}
+
+	public virtual void kill()
+	{
+		m_isRunning = false;
+	}
+	#endregion
+
+	#region Protected Functions
+	protected virtual IEnumerator doFlow()
+	{
+		while(m_isRunning)
+		{
+			// skip one frame for pause at the start
+			yield return null;
+
+			if(isPaused)
+			{
+				// paused do nothing
+				yield return null;
+			}
+			else
+			{
+				// no paused
+				yield return m_coroutine.Current;
+
+				if(!m_coroutine.MoveNext())
+				{
+					m_isRunning = false;
+				}
+			}
+		}
+	}
+
+	protected virtual IEnumerator untilDoneFlow()
+	{
+		while(m_isRunning)
+		{
+			yield return null;
+		}
+	}
+	#endregion
+}
