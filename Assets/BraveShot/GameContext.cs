d5ba316f6f3e14b09cb1e7de8cb7de11a6cb80cb
maxcat <tanli1622@gmail.com>
2016-02-07 02:39:01 +0800

    [DEV] add base input for braveshot.

---
+++ Assets/BraveShot/GameContext.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameContext : MonoBehaviour {
+
+	#region Fields
+	[SerializeField] protected List<GameObject> 				m_playerBalls;
+	[SerializeField] protected List<GameObject>					m_opponentBalls;
+	#endregion
+
+	#region Getter and Setter
+	public List<GameObject> playerBalls
+	{
+		get { return m_playerBalls;}
+	}
+
+	public List<GameObject> opponentBalls
+	{
+		get { return m_opponentBalls;}
+	}
+	
+	#endregion
+}
