67009916114b2411a2fca37763498aa22c50f7ae
maxcat <tanli1622@gmail.com>
2016-02-09 03:16:55 +0800

    [DEV] add terrain block handler.

--- Assets/BraveShot/TerrainBlocks/TerrainBlockController.cs
+++ Assets/BraveShot/TerrainBlocks/TerrainBlockController.cs
@@ -1,20 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BallControl : MonoBehaviour {
-
-	#region Fields
-	[SerializeField] protected ForceMode		m_releaseForceMode;
-	[SerializeField] float						m_multiply = 1f;
-	protected Rigidbody							m_body;
-	protected SphereCollider					m_collider;
-	#endregion
+public class TerrainBlockController : MonoBehaviour {
 
 	#region Override MonoBehaviour
-	void Awake () {
-		init();
-	}
-
 	// Use this for initialization
 	void Start () {
 	
@@ -32,37 +21,28 @@ public class BallControl : MonoBehaviour {
 			Debug.DrawRay(contact.point, contact.normal * 10, Color.white);
 		}
 	}
-
+	
 	void OnCollisionStay(Collision collisionInfo) {
 		foreach (ContactPoint contact in collisionInfo.contacts) {
 			Debug.DrawRay(contact.point, contact.normal * 10, Color.white);
 		}
 	}
-
+	
 	void OnCollisionExit(Collision collisionInfo) {
-//		print("No longer in contact with " + collisionInfo.transform.name);
+		print("No longer in contact with " + collisionInfo.transform.name);
 	}
-
+	
 	void OnTriggerEnter(Collider other) {
-	}
 
+		other.rigidbody.AddForce(Vector3.forward * 100 + other.transform.position, ForceMode.Force);
+	}
+	
 	void OnTriggerExit(Collider other) {
 	}
-
+	
 	void OnTriggerStay(Collider other) {
+		other.rigidbody.AddForce(Vector3.forward * 100 + other.transform.position, ForceMode.Force);
 	}
-	#endregion
 
-	#region Public API
-	public virtual void init()
-	{
-		m_body = GetComponent<Rigidbody>();
-	}
-	public virtual void releaseBall(Vector3 input)
-	{
-		m_body.AddForce(new Vector3(input.x, 0f, input.y).normalized * m_multiply + transform.position, m_releaseForceMode);
-
-		m_body.AddTorque(transform.up * 100f, m_releaseForceMode);
-	}
 	#endregion
 }
