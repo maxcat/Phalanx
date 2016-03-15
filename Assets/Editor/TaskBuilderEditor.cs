a4c116c44d1427c79311044d59c2e2644b3a5996
Li Tan <li.tan@gumi.sg>
2016-03-15 19:05:19 +0800

    [DEV] add task editors.

---
+++ Assets/Editor/TaskBuilderEditor.cs
@@ -0,0 +1,27 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityEditor;
+//
+//
+//[CustomEditor(typeof(TaskCollectionData))]
+//public class TaskBuilderEditor : Editor {
+//
+//	#region Override Editor Functions
+//	public override void OnInspectorGUI ()
+//	{
+//		DrawDefaultInspector();
+//
+//		TaskCollectionData collection = (TaskCollectionData)target;
+//
+//		if(GUILayout.Button("Open Editor"))
+//		{
+////			TaskEditorWindow.showWindow();
+//
+//			TaskEditorWindow window = EditorWindow.GetWindow<TaskEditorWindow>();
+//
+//			window.init(collection);
+//		}
+//
+//	}
+//	#endregion
+//}
