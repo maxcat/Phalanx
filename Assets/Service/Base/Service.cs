ae79806a5a92633d5afb9727e9394b1b752b8aa1
maxcat <tanli1622@gmail.com>
2016-03-28 02:39:29 +0800

    [DEV] add interface for singleton for init, pre remove, game pause and scene change.

--- Assets/Service/Base/Service.cs
+++ Assets/Service/Base/Service.cs
@@ -1,25 +1,99 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Service : MonoBehaviour {
 
-
-
 	#region Override MonoBehaviour
-	// Use this for initialization
+	void Awake () {
+		// init all the static service inside the prefab.
+		List<Singleton> singletonList = getSingletonList();
+
+		if(singletonList != null)
+		{
+			for(int i = 0; i < singletonList.Count; i ++)
+			{
+				singletonList[i].init();
+			}
+		}
+	}
+
+	void OnApplicationPause	()
+	{
+		List<Singleton> singletonList = getSingletonList();
+
+		if(singletonList != null)
+		{
+			for(int i = 0; i < singletonList.Count; i ++)
+			{
+				singletonList[i].onGamePaused();
+			}
+		}
+	}
+
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnLevelWasLoaded (int level) {
+
+		List<Singleton> singletonList = getSingletonList();
+
+		if(singletonList != null)
+		{
+			for(int i = 0; i < singletonList.Count; i ++)
+			{
+				singletonList[i].onSceneChanged(level);
+			}
+		}
+
+		List<Singleton> removeList = getInPersistSingletonList();
+		if(removeList != null)
+		{
+			for(int i = 0; i < removeList.Count; i ++)
+			{
+				removeList[i].preRemoved();
+				GameObject.Destroy(removeList[i]);
+			}
+		}
 	}
 	#endregion
 
 	#region Static Fields
 	static GameObject m_serviceObject;
 	#endregion
+
+#region Protected Functions
+	protected List<Singleton> getSingletonList()
+	{
+		List<Singleton> wholeList = new List<Singleton>(this.GetComponents<Singleton>());
+		return wholeList;
+	}
+
+	protected List<Singleton> getInPersistSingletonList()
+	{
+		List<Singleton> wholeList = getSingletonList();
+
+		if(wholeList != null)
+			return wholeList.FindAll(singleton=>!singleton.isPersist);
+		return null;
+	}
+
+	protected List<Singleton> getPersistSingletonList()
+	{
+		List<Singleton> wholeList = getSingletonList();
+
+		if(wholeList != null)
+		{
+			return wholeList.FindAll(singleton=>singleton.isPersist);
+		}
+		return null;
+	}
+#endregion
 	
 	#region Static Public API
 	public static T get<T>() where T : Singleton
@@ -35,6 +109,8 @@ public class Service : MonoBehaviour {
 		if(service == null)
 		{
 			service = m_serviceObject.AddComponent<T>();
+			// init the singleton
+			service.init();
 		}
 
 		return service;
@@ -47,7 +123,10 @@ public class Service : MonoBehaviour {
 
 		T service = m_serviceObject.GetComponent<T>();
 		if(service != null)
+		{
+			service.preRemoved();
 			GameObject.Destroy(service);
+		}
 	}
 	#endregion
 }
