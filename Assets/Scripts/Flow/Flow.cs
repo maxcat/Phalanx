7db4d4d53559236285c3f01c80eb9d640046a250
Li Tan <li.tan@gumi.sg>
2016-06-09 17:30:54 +0800

    [DEV] add base flows.

---
+++ Assets/Scripts/Flow/Flow.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flow : IEnumerator {
+
+#region Fields
+	protected float 		flowSpeed;
+	protected IEnumerator 		source;
+	protected Flow 			nextFlow;
+	protected bool 			isFlowStarted = false;
+#endregion
+
+#region Constructor
+	public Flow(IEnumerator flow)
+	{
+		source = flow;
+		nextFlow = null;
+	}
+
+	public Flow()
+	{
+		source = main();
+		nextFlow = null;
+	}
+#endregion
+
+#region Getter and Setter
+	public Flow NextFlow
+	{
+		get { return nextFlow; }
+	}
+
+	public bool IsFlowStarted
+	{
+		get { return isFlowStarted; }
+	}
+#endregion
+
+#region Implement Inteface
+	public virtual bool MoveNext()
+	{
+		isFlowStarted = true;
+		return source != null ? source.MoveNext() : false;
+	}
+
+	public virtual object Current
+	{
+		get {
+			return source != null ? source.Current : null;
+		}
+	}
+
+	public void Reset()
+	{
+	}
+
+#endregion
+
+#region Virtual Functions
+	protected virtual IEnumerator main()
+	{
+		yield return null;
+	}
+
+	public virtual void SetFlowSpeed()
+	{
+
+	}
+#endregion
+
+#region Public API
+	public void SetNext(Flow next)
+	{
+		nextFlow = next;	
+	}
+
+	public void SetNext(IEnumerator next)
+	{
+		if(next != null)
+			nextFlow = new Flow(next);
+	}
+#endregion
+}
---
+++ Assets/Scripts/Flow/SequentialFlow.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SequentialFlow : Flow {
+
+#region Constructor
+	protected Flow 			currentFlow;
+	protected Flow 			tailFlow;
+#endregion
+
+#region Constructor
+	public SequentialFlow()
+	{
+		currentFlow = null;
+		tailFlow = null;
+	}
+#endregion
+
+#region Implement Virtual Functions 
+	public override bool MoveNext()
+	{
+		isFlowStarted = true;
+		if(currentFlow == null)
+			return false;
+
+		if(currentFlow.MoveNext())
+		{
+			return true;
+		}
+		else
+		{
+			if(currentFlow.NextFlow != null)
+			{
+				currentFlow = currentFlow.NextFlow;
+				return currentFlow.MoveNext();
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	public override object Current
+	{
+		get { 
+			return currentFlow.Current;
+	       	}
+	}
+#endregion
+
+#region Public API
+	public void Add(Flow next)
+	{
+		if(next == null)
+			return;
+
+		if(tailFlow != null)
+		{
+			tailFlow.SetNext(next);
+			tailFlow = next;
+		}
+		else
+		{
+			currentFlow = next;
+			tailFlow = next;
+		}
+	}
+
+	public void Add(IEnumerator nextEnumerator)
+	{
+		if(nextEnumerator == null)
+			return;
+		Flow next = new Flow(nextEnumerator);
+
+		if(tailFlow != null)
+		{
+			tailFlow.SetNext(next);
+			tailFlow = next;
+		}
+		else
+		{
+			currentFlow = next;
+			tailFlow = next;
+		}
+	}
+
+	public void AddToHead(Flow head)
+	{
+		if(head == null)
+			return;
+
+		if(currentFlow != null && currentFlow.IsFlowStarted)
+		{
+			head.SetNext(currentFlow.NextFlow);
+			currentFlow.SetNext(head);
+		}
+		else
+		{
+			if(tailFlow != null)
+				head.SetNext(currentFlow);
+			else
+				tailFlow = head;
+			currentFlow = head;
+		}
+	}
+
+	public void AddToHead(IEnumerator headEnumerator)
+	{
+		if(headEnumerator == null)
+			return;
+
+		Flow head = new Flow(headEnumerator);
+		if(currentFlow != null && currentFlow.IsFlowStarted)
+		{
+			head.SetNext(currentFlow.NextFlow);
+			currentFlow.SetNext(head);
+		}
+		else
+		{
+			if(tailFlow != null)
+				head.SetNext(currentFlow);
+			else
+				tailFlow = head;
+			currentFlow = head;
+		}
+	}
+#endregion
+
+}
